5c1599c6fd2a6aa63910c34382f8cf3730f49135
sku272 <skumar273@sapient.com>
2018-03-07 20:03:07 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import Error404 from './components/Error404';
 import registerServiceWorker from './registerServiceWorker';
 import {Router, Route, hashHistory} from 'react-router';
 
-ReactDOM.render(<Router history={hashHistory}>
+ReactDOM.render(
+    <Router history={hashHistory}>
         <Route path="/" component={Home}> </Route>
         <Route path="/about" component={About}></Route>
         <Route path="/app" component={App} > </Route> 
-        <Route path="*" component={Error404}> </Route>
-        
+        <Route path="*" component={Error404}> </Route>        
     </Router>, document.getElementById('root'));
 registerServiceWorker();
